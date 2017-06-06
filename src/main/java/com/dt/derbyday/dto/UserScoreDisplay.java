cc992689e0fe1eb994369908fb73103da30a4c5f
luoyinqi <luoyinqi@niwodai.net>
2017-06-06 09:15:21 +0800

    排名显示

---
+++ src/main/java/com/dt/derbyday/dto/UserScoreDisplay.java
@@ -0,0 +1,33 @@
+package com.dt.derbyday.dto;
+
+public class UserScoreDisplay {
+	private String username;
+	private String phone;
+	private String address;
+	private int score;
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public int getScore() {
+		return score;
+	}
+	public void setScore(int score) {
+		this.score = score;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+}
